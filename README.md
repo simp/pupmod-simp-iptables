b9ab7d1e85c7164795be24f7607e022db78e6054
Chris Tessmer <chris.tessmer@onyxpoint.com>
2015-06-29 19:29:29 -0400

    (SIMP-56) use hiera as default for iptables::disable
    Before this patch, iptables::disable did not reference the hiera global
    'use_iptables' (which is set from the intial questionnaire by simp
    config), nor honor the iptables::disable parameter with IPv4 firewalls.
    
    With this commit, iptables::disable now controls the management of IPv4
    firewalls and refences hiera('use_iptables') to set its default value.
    
    This commit also introduces limited spec and acceptance (beaker) tests.
    
    It is possible to run beaker tests using the modules under
    spec/fixtures/modules (installed via `rake spec_prep`) by running
    acceptance tests with the environment variable setting:
    
      BEAKER_use_fixtures_dir_for_modules=yes
    
    SIMP-56 #close
    
    Change-Id: Ib1780b93896ee73b0605074a5f865eea87deddd4

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
+[![Build Status](https://travis-ci.org/pupmod-simp-iptables.svg?branch=master)](https://travis-ci.org/pupmod-simp-iptables)
+
+
 ## Work in Progress
 
 Please excuse us as we transition this code into the public domain.
 
 Downloads, discussion, and patches are still welcome!
+
+
+
+## Acceptance tests
+
+To run the system tests, you need [Vagrant](https://www.vagrantup.com/) installed. Then, run:
+
+    bundle exec rake acceptance
+
+Some environment variables may be useful:
+
+    BEAKER_debug=true
+    BEAKER_provision=no
+    BEAKER_destroy=no
+    BEAKER_use_fixtures_dir_for_modules=yes
+
+* The `BEAKER_debug` variable shows the commands being run on the STU and their output.
+* `BEAKER_destroy=no` prevents the machine destruction after the tests finish so you can inspect the state.
+* `BEAKER_provision=no` prevents the machine from being recreated. This can save a lot of time while you're writing the tests.
+* `BEAKER_use_fixtures_dir_for_modules=yes` causes all module dependencies to be loaded from the `spec/fixtures/modules` directory, based on the contents of `.fixtures.yml`.  The contents of this directory are usually populated by `bundle exec rake spec_prep`.  This can be used to run acceptance tests to run on isolated networks.
---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper_acceptance'
+
+test_name "iptables class"
+
+describe 'iptables' do
+  let(:manifest) {
+    <<-EOS
+      class { 'iptables': }
+
+      # Ironically, if iptables applies correctly, its default settings will
+      # deny Vagrant access via SSH.  So, it is neccessary for beaker to also
+      # define a rule that permit SSH access from the standard Vagrant subnets:
+      iptables::add_tcp_stateful_listen { 'allow_sshd':
+        client_nets => ['10.0.2.0/16'],  # Standard Beaker/Vagrant subnet
+        dports      => '22',
+        require     => Class['iptables']
+      }
+    EOS
+  }
+
+  it 'should work with no errors' do
+    apply_manifest(manifest, :catch_failures => true)
+  end
+
+  it 'should be idempotent' do
+    apply_manifest(manifest, :catch_changes => true)
+  end
+
+  it 'should install the iptables package' do
+    expect(shell("puppet resource package iptables").stdout).to_not include('absent')
+  end
+
+  it 'should ensure that the iptables service is running' do
+    expect(shell("puppet resource service iptables").stdout).to include('running')
+  end
+
+  it 'should include a single TCP rule' do
+    shell("test `iptables-save  | grep ' -p tcp' | wc -l` -eq 1", :acceptable_exit_codes => 0)
+  end
+
+  it 'should allow port 22' do
+    expect(shell("iptables-save  | grep ' -p tcp'").stdout).to include(' --dport', ' 22')
+  end
+end
+
+### describe 'iptables { service_ensure => stopped }' do
+###   let(:manifest) {
+###     <<-EOS
+###       class { 'iptables':
+###         service_ensure => stopped,
+###       }
+###     EOS
+###   }
+###
+###   it 'should stop the iptables service' do
+###     expect(shell("puppet resource service iptables").stdout).to include('stopped')
+###   end
+### end
+###
+### describe 'iptables { package_ensure => absent }' do
+###   # If the service parameter is absent, removing the package makes each application fail.
+###   let(:manifest) {
+###     <<-EOS
+###       class { 'iptables':
+###         package_ensure => absent,
+###         service_ensure => stopped,
+###       }
+###     EOS
+###   }
+###
+###
+###   it 'should disable the iptables service' do
+###     expect(shell("puppet resource service iptables").stdout).to include("enable => 'false'")
+###   end
+### end
+
