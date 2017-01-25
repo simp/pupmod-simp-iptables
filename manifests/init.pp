0df308fdfd92d9bbff3f90574955ca7e612cf5ff
Nick Miller <nick.miller@onyxpoint.com>
2017-01-25 17:00:52 -0500

    (SIMP-1897) Open iptables ports from hiera (#28)
    SIMP-1897 #close

--- manifests/init.pp
+++ manifests/init.pp
@@ -64,6 +64,19 @@
 #   Add rules to ``PREROUTING`` that will prevent spoofed packets from
 #   ``localhost`` addresses from reaching your system
 #
+# @param ports [Hash]
+#   A hash with structure as defined below that will open ports based on the
+#   structure of the hash.
+#   @example An example section of hieradata:
+#     iptables::ports:
+#       defaults:
+#         apply_to: ipv4
+#       80:
+#       53:
+#         proto: udp
+#       443:
+#         apply_to: ipv6
+#
 # @author Trevor Vaughan <tvaughan@onyxpoint.com>
 # @author Chris Tessmer  <chris.tessmer@onyxpoint.com>
 #
@@ -76,7 +89,8 @@ class iptables (
   Array[String]                   $ignore                     = [],
   Boolean                         $default_rules              = true,
   Boolean                         $scanblock                  = false,
-  Boolean                         $prevent_localhost_spoofing = true
+  Boolean                         $prevent_localhost_spoofing = true,
+  Optional[Hash]                  $ports                      = undef
 ) {
 
   if $enable != 'ignore' {
@@ -105,4 +119,48 @@ class iptables (
       }
     }
   }
+
+  if $ports {
+    # extract defaults and remove that hash from iteration
+    if $ports['defaults'].is_a(Hash) {
+      $defaults  = $ports['defaults']
+      $raw_ports = $ports - 'defaults'
+    }
+    else {
+      $defaults  = {}
+      $raw_ports = $ports
+    }
+
+    # https://docs.puppet.com/puppet/latest/reference/lang_resources_advanced.html#implementing-the-createresources-function
+    $raw_ports.each |$port, $options| {
+      $name_to_param = {
+        'dports' => [$port],
+      }
+
+      if $options.is_a(Hash) {
+        $proto = $options['proto']
+        $args  = ($options - 'proto') + $name_to_param
+      }
+      else {
+        $proto = 'tcp'
+        $args  = $name_to_param
+      }
+
+      case $proto {
+        default: {
+          iptables::listen::tcp_stateful {
+            default:        * => $defaults;
+            "port_${port}": * => $args;
+          }
+        }
+        'udp': {
+          iptables::listen::udp {
+            default:        * => $defaults;
+            "port_${port}": * => $args;
+          }
+        }
+      }
+    }
+  }
+
 }
