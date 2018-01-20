702a720959b78aa3cfcb3d7fe6ae323deb8c8388
Nick Miller <nick.miller@onyxpoint.com>
2018-01-20 11:39:47 -0500

    Fix bug where proto is in defaults hash

--- manifests/ports.pp
+++ manifests/ports.pp
@@ -1,5 +1,5 @@
 # A define to allow for the standardization of the
-#  iptables::ports syntax across modules 
+#  iptables::ports syntax across modules
 #
 # @param ports
 #   A hash with structure as defined below that will open ports based on the
@@ -27,7 +27,6 @@ define iptables::ports (
     $raw_ports = $ports
   }
 
-  # https://docs.puppet.com/puppet/latest/reference/lang_resources_advanced.html#implementing-the-createresources-function
   $raw_ports.each |$port, $options| {
     $_port = Integer.new($port)
     $name_to_param = {
@@ -35,24 +34,36 @@ define iptables::ports (
     }
 
     if $options.is_a(Hash) {
-      $proto = $options['proto']
-      $args  = ($options - 'proto') + $name_to_param
+      if $options['proto'] {
+        $proto = $options['proto']
+      }
+      else {
+        $proto = $defaults['proto']
+      }
+      $_defaults = $defaults - 'proto'
+      $args      = ($options - 'proto') + $name_to_param
     }
     else {
-      $proto = 'tcp'
+      if $defaults['proto'] {
+        $proto = $defaults['proto']
+      }
+      else {
+        $proto = 'tcp'
+      }
+      $_defaults = $defaults - 'proto'
       $args  = $name_to_param
     }
 
     case $proto {
       default: {
         iptables::listen::tcp_stateful {
-          default:        * => $defaults;
+          default:        * => $_defaults;
           "port_${port}": * => $args;
         }
       }
       'udp': {
         iptables::listen::udp {
-          default:        * => $defaults;
+          default:        * => $_defaults;
           "port_${port}": * => $args;
         }
       }
--- spec/defines/ports_spec.rb
+++ spec/defines/ports_spec.rb
@@ -8,11 +8,29 @@ describe "iptables::ports", :type => :define do
       end
       let(:title) { 'iptables' }
 
+      context 'a hash without a default section' do
+        let(:params) {{
+          'ports' => {
+            '80': nil,
+            '53': {
+              'proto' => 'udp'
+            },
+            '443' => {
+              'apply_to' => 'ipv6'
+            }
+          }
+        }}
+        it { is_expected.to create_iptables__listen__tcp_stateful('port_80').with(apply_to: 'auto') }
+        it { is_expected.to create_iptables__listen__udp('port_53').with(apply_to: 'auto') }
+        it { is_expected.to create_iptables__listen__tcp_stateful('port_443').with(apply_to: 'ipv6') }
+      end
+
       context 'containing a defaults section' do
         let(:params) {{
           'ports' => {
             'defaults' => {
-              'apply_to' => 'ipv4'
+              'apply_to' => 'ipv4',
+              'proto'    => 'tcp'
             },
             '80': nil,
             '53': {
@@ -25,23 +43,6 @@ describe "iptables::ports", :type => :define do
         }}
         it { is_expected.to create_iptables__listen__tcp_stateful('port_80').with(apply_to: 'ipv4') }
         it { is_expected.to create_iptables__listen__udp('port_53').with(apply_to: 'ipv4') }
-        it { is_expected.to create_iptables__listen__tcp_stateful('port_443').with(apply_to: 'ipv6') }
-      end
-
-      context 'a hash without a default section' do
-        let(:params) {{
-          'ports' => {
-            '80': nil,
-            '53': {
-              'proto' => 'udp'
-            },
-            '443' => {
-              'apply_to' => 'ipv6'
-            }
-          }}
-        }
-        it { is_expected.to create_iptables__listen__tcp_stateful('port_80').with(apply_to: 'auto') }
-        it { is_expected.to create_iptables__listen__udp('port_53').with(apply_to: 'auto') }
         it { is_expected.to create_iptables__listen__tcp_stateful('port_443').with(apply_to: 'ipv6') }
       end
 
