ed2f1b0c0178af5be61257cfbb129fb0f68dadbd
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-03-10 10:07:14 -0500

    (SIMP-9432) Fix EL8 packages
    - Fixed
      - Moved the service-relevant files out of `install` and into `service`
      - Ensure that EL8+ installs `iptables-service` instead of trying to install
        the EL7 packages
      - Call `iptables::install` in all enabled modes since `firewalld` may require
        the underlying packages

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,8 @@
 # Manage the IPTables and IP6Tables services
 #
+# This also installs fallback startup scripts that come into play should the
+# regular processes fail to start due to a race condition with DNS.
+#
 # @param enable
 #   Enable IPTables
 #
@@ -25,21 +28,61 @@ class iptables::service (
       $_enable = false
     }
 
+    file { '/etc/init.d/iptables':
+      ensure  => 'file',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => file("${module_name}/iptables"),
+      seltype => 'iptables_initrc_exec_t'
+    }
+
+    # --------------------------------------------------
+    # Set the iptables startup script to fail safe.
+    #
+    file { '/etc/init.d/iptables-retry':
+      ensure  => 'file',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0744',
+      content => file("${module_name}/iptables-retry"),
+      seltype => 'iptables_initrc_exec_t'
+    }
+
     service { 'iptables':
       ensure     => $_ensure,
       enable     => $_enable,
       hasrestart => false,
       restart    => '/sbin/iptables-restore /etc/sysconfig/iptables || ( /sbin/iptables-restore /etc/sysconfig/iptables.bak && exit 3 )',
       hasstatus  => true,
+      require    => File['/etc/init.d/iptables'],
       provider   => 'redhat'
     }
 
     service { 'iptables-retry':
       enable   => $_enable,
+      require  => File['/etc/init.d/iptables-retry'],
       provider => 'redhat'
     }
 
     if $ipv6 and $facts['ipv6_enabled'] {
+      file { '/etc/init.d/ip6tables':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        seltype => 'iptables_initrc_exec_t',
+        content => file("${module_name}/ip6tables")
+      }
+
+      file { '/etc/init.d/ip6tables-retry':
+        ensure  => 'file',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0744',
+        seltype => 'iptables_initrc_exec_t',
+        content => file("${module_name}/ip6tables-retry")
+      }
       service { 'ip6tables':
         ensure     => $_ensure,
         enable     => $_enable,
