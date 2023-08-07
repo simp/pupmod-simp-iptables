f0fd10411ad4a9c773addb68a601684151ace662
Steven Pritchard <steve@sicura.us>
2023-08-07 11:26:41 -0500

    Remove gating logic (#117)
    Remove the logic from the `iptables` class that prevents the class
    from using `firewalld` when it has been requested (via
    `iptables::use_firewalld`) but has not been installed yet.

--- manifests/listen/tcp_stateful.pp
+++ manifests/listen/tcp_stateful.pp
@@ -77,7 +77,7 @@ define iptables::listen::tcp_stateful (
 ) {
   include 'iptables'
 
-  if $iptables::firewalld_mode {
+  if $iptables::use_firewalld {
     simplib::assert_optional_dependency($module_name, 'simp/simp_firewalld')
 
     simp_firewalld::rule { "tcp_${name}":
